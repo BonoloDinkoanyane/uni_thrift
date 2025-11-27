4cb8c23bce0d119ae4ef2d9762d92ded99de4274
BonoloDinkoanyane <bonolodinkoa@icloud.com>
2025-11-28 01:10:11 +0200

    initialised prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,7 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../lib/generated/prisma";
+
+//singleton code prevents many instances of PrismaClient in development
+// so that we don't exceed database connection limits
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
